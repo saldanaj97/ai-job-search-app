e196e2a19fab21acc43a540f7827aed08a0553ec
Juan Saldana <saldanaj97@gmail.com>
2024-08-19 12:26:08 -0500

    linting cleanup

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,22 +1,23 @@
 "use client";
 
 import {ThemeProvider as NextThemesProvider} from "next-themes";
-import {type ThemeProviderProps} from "next-themes/dist/types";
+import {ReactNode} from "react";
 
 interface ThemeProviderProps {
     attribute?: string,
     defaultTheme?: string,
     enableSystem?: boolean,
     disableTransitionOnChange?: boolean
+    children: ReactNode,
 }
 
 export function ThemeProvider({
                                   children,
-                                  ...props,
                                   attribute,
                                   defaultTheme,
                                   enableSystem,
-                                  disableTransitionOnChange
+                                  disableTransitionOnChange,
+                                  ...props
                               }: ThemeProviderProps) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
