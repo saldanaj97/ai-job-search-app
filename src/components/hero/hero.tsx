f1ae40017072aab5c95bee6f031e0304af75407f
Juan Saldana <saldanaj97@gmail.com>
2024-08-21 13:00:32 -0500

    refactor: Update hero component with new text and buttons
    - Update the text and buttons in the hero component to improve the messaging and user experience on the landing page.

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,28 +3,29 @@
 import { TypingEffect } from '~/components/hero/TypingEffect';
 import { Button } from '~/components/ui/button';
 
-// TODO: Fix the typing effect to stop moving the text down for improved user reading flow
 export default function Hero() {
   return (
     <div className="container relative z-30 flex max-w-3xl flex-1 flex-col items-center justify-center text-center">
-      {/* Hero Heading */}
-      <h1 className="relative z-10 mb-4 text-5xl font-bold tracking-tight sm:h-24 md:h-fit md:w-screen md:text-7xl lg:h-fit lg:text-8xl">
-        Tired of being{' '}
-        <TypingEffect word="overlooked?" simulateErrors={false} />
-      </h1>
+      <div className="flex flex-col gap-6">
+        <h1 className="z-10 text-[56px] font-bold tracking-tight">
+          Tired of being{' '}
+          <TypingEffect word="rejected?" simulateErrors={false} />
+        </h1>
+        <p className="z-10 w-full text-[18px] font-normal tracking-tight">
+          Welcome to WannabeHired.ai, your AI-powered job application assitant.
+          We optimize and organize your job search to increase your chances of
+          landing interviews.
+        </p>
+      </div>
 
-      {/* Hero Subheading */}
-      <p className="relative z-10 mb-8 w-full text-xl font-normal tracking-tight sm:text-xl sm:font-semibold">
-        Our AI-powered assistant tailors job recommendations, crafts compelling
-        resumes and cover letters, and provides real-time feedback to enhance
-        your applications and chances of getting hired. Start your journey to
-        career success today!
-      </p>
-
-      {/* Call to Action Button */}
-      <Button className="relative z-10 bg-primary text-white shadow-lg">
-        Get started!
-      </Button>
+      <div className="flex flex-row gap-4 pt-4">
+        <Button className="relative z-10 bg-primary px-6 text-white shadow-lg">
+          Get started
+        </Button>
+        <Button className="relative z-10 bg-secondary px-6 text-white shadow-lg">
+          Learn more
+        </Button>
+      </div>
     </div>
   );
 }
