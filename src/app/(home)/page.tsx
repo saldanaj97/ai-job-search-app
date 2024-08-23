f695a71888a730051afdd1708219f9aa95858dbc
Juan Saldana <saldanaj97@gmail.com>
2024-08-23 10:19:06 -0500

    refactor: Import CallToAction component in home page
    The CallToAction component was imported from the correct location in the home page file.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,8 @@
 import { cookies } from 'next/headers';
 import Hero from '~/components/hero/hero';
+import { CallToAction } from '~/components/ui/cta';
 import { createClient } from '~/utils/supabase/server';
 import DashboardPage from '../dashboard/page';
-import { CallToAction } from './cta';
 import HowItWorks from './how-it-works';
 import MainFeatures from './main-features';
 
